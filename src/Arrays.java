b0f69d4ca58ebddf03aec1caebcc2a8334f0d164
6eJapycc <dsenazatskij@gmail.com>
2022-12-28 21:57:08 +0300

    1

---
+++ src/Arrays.java
@@ -0,0 +1,45 @@
+public class Arrays {
+    public static void main(String[] args) {
+//        int number = 1;
+//        int[][] array = new int[10][10];
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                array[i][j] = number++;
+//            }
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println("");
+//        }
+
+
+//        int[][] array = new int[10][10];
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            for (int j = i; j < array.length; j++) {
+//                array[i][j] = 1;
+//            }
+//        }
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            for (int j = array.length - 1; j >= 0; j--) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println("");
+//        }
+
+//        int[][] array = new int[10][10];
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i; j < array.length; j++) {
+//                array[i][j] = 1;
+//            }
+//        }
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            for (int j = i; j < array.length; j++) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println("");
+//        }
+
+    }
+}
